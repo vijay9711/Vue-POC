e450cc00ade796aa449abd5a24e2090549d78810
manovijay1997 <vijay.m@diatoz.com>
2019-06-28 00:58:20 +0530

    add card ,increment, decrement button in card and then count

--- src/views/shoppinglist.vue
+++ src/views/shoppinglist.vue
@@ -4,17 +4,30 @@
     <h1>shopping list</h1>
     <div class="row">
       <mdb-input label="Item" type="text" v-model="item" class="itemInput m-auto"/>
-      <mdb-btn size="sm" @click="addItem" color="primary m-auto addItem">Add Item</mdb-btn>
+      <mdb-btn size="sm" type="button" @click="addItem" color="primary m-auto addItem">Add Item</mdb-btn>
     </div>
     <div class="row listItems">
-      <div class="card" v-for="item in itemName">
+      <div class="col-12 card mt-2" v-for="item in itemName" :key="item.id">
         <div class="item card-body">
-          <p>{{item}}</p>
+          <label class="w-25">{{item.items}}</label>
+          <mdb-btn
+            size="sm"
+            :id="item.id"
+            type="button"
+            @click="increment($event)"
+            color="success ml-5 increment"
+          >+</mdb-btn>
+          <mdb-btn
+            size="sm"
+            :id="item.id"
+            type="button"
+            @click="decrement($event)"
+            color="primary ml-5 decrement"
+          >-</mdb-btn>
+          <span class="ml-5">count {{item.count}}</span>
         </div>
       </div>
     </div>
-
-    <div class="row listItems"></div>
   </div>
 </template>
 <script>
@@ -22,6 +35,7 @@ import Nav from "../components/navbar/navbar.vue";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbvue/build/css/mdb.css";
 import { mdbInput, mdbBtn } from "mdbvue";
+import { constants } from "crypto";
 export default {
   components: {
     Nav,
@@ -31,13 +45,42 @@ export default {
   data: function() {
     return {
       itemName: [],
-      item: ""
+      item: "",
+      count: 0
     };
   },
   methods: {
     addItem() {
-      this.itemName.push(this.item);
+      this.itemName.push({
+        items: this.item,
+        id: this.itemName.length + 1,
+        count: 0
+      });
       console.log("item ", this.itemName);
+    },
+    increment(event) {
+      this.itemName.map(item => {
+        if (item.id.toString() === event.target.id.toString()) {
+          item.count = item.count + 1;
+          console.log("it is if");
+        } else {
+          console.log("it is else");
+        }
+      });
+      console.log("increment", this.itemName);
+    },
+    decrement(event) {
+      this.itemName.map(item => {
+        if (item.id.toString() === event.target.id.toString()) {
+          if (item.count === 0) {
+            item.count = 0;
+          } else if (item.count !== 0) {
+            item.count = item.count - 1;
+          }
+        } else {
+          console.log("it is else");
+        }
+      });
     }
   }
 };
@@ -48,6 +91,7 @@ export default {
   width: auto;
   height: auto;
   background-color: aliceblue;
+  padding: 2rem;
 }
 .title {
   margin-left: 0px !important;
@@ -65,7 +109,13 @@ export default {
 .item {
   align-content: center;
   width: auto;
-  height: auto;
-  margin: auto;
+  margin-left: 10rem;
+  margin-right: 10rem;
+  text-align: left;
+  /* border: 2px solid black; */
+}
+.increment {
+}
+.decrement {
 }
 </style>
