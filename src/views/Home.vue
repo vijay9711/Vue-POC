601c8fb929de8ddfac9d9f0c78360a824a8bbf42
manovijay1997 <vijay.m@diatoz.com>
2019-07-07 14:01:33 +0530

    rename the helloworld file and add login and sign up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WelcomePage />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import WelcomePage from '@/components/WelcomePage.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home',
+    components: {
+      WelcomePage
+    }
   }
-}
 </script>
