92245284e613d21becfbcf80b417c401482d02b4
vijay <vijaysm97@gmail.com>
2019-11-21 13:01:22 +0530

    server removed

--- server/routes/index.js
+++
@@ -1,5 +0,0 @@
-const userController = require("../controllers/userController.js");
-module.exports = function(app) {
-  app.post("/index", userController.userLogin);
-  app.post("/login", userController.userSignup);
-};
--- server/routes/socialUser.js
+++
@@ -1,4 +0,0 @@
-const SocialUser = require("../controllers/socialAuthUser.js");
-module.exports = function(app) {
-  app.get("/socialUser/:id", SocialUser.socialAuthUser);
-};
